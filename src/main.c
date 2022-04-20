e6f3e8d5d340d67a77b7c87c2d70320083f4e143
rxtz <62116831+rxtz@users.noreply.github.com>
2022-04-20 18:33:55 -0500

    subir

---
+++ src/main.c
@@ -0,0 +1,50 @@
+/**
+ * Angle Converter
+ * @author rxtz
+ * @date 2022-04-15
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  double a; // bebesita
+
+  while (1) {
+    char inp[16];
+    printf("Angle: ");
+    scanf("%s", inp);
+
+    if ((a = strtod(inp, NULL))) {
+      break;
+    }
+  }
+
+  char unit = ' ';
+
+  while (unit != 's' && unit != 'c' && unit != 'r') {
+    printf("Sexagesimal/Centesimal/Radian [s/c/r]: ");
+    scanf(" %s", &unit);
+  }
+
+  double sex, cen, rad;
+
+  // Expression is not assignable. C++ valid
+  // ((unit == 's') ? sex : (unit == 'c') ? cen : rad) = a;
+
+  (unit == 's') ? sex = a : (unit == 'c') ? cen = a : (rad = a);
+
+  if (unit != 's') {
+    sex = (unit == 'c') ? cen * 9 / 10 : rad * 180;
+  }
+  if (unit != 'c') {
+    cen = sex * 10 / 9;
+  }
+  if (unit != 'r') {
+    rad = sex / 180;
+  }
+
+  printf("%.2lf°\n%.2lfᵍ\n%.2lf rad\n", sex, cen, rad);
+
+  return 0;
+}
